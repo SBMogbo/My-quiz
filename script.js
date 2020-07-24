1de7b382955115499cdae3f0935de18812094c58
Steven Brooks Mogbo <stevenmogbo@yahoo.com>
2020-07-23 22:47:23 -0400

    Updated Javascript

--- script.js
+++ script.js
@@ -1,70 +1,92 @@
-var questions= [
+var questions = [
   {
-    title:"What is My favorite color?",
-    choices:["Green","Yellow","Blue","Red"],
-    answer:"Blue"
+    title: "What is My favorite color?",
+    choices: ["Green", "Yellow", "Blue", "Red"],
+    answer: "Blue"
   },
   {
-    title:"What month was I born in?",
-    choices:["April","July","March","December"],
-    answer:"March"
+    title: "What month was I born in?",
+    choices: ["April", "July", "March", "December"],
+    answer: "March"
   },
   {
-    title:"Who is my favorite superhero",
-    choices:["Superman","Black Panther","Static","Spider-Man"],
-    answer:"Spider-Man"
+    title: "Who is my favorite superhero",
+    choices: ["Superman", "Black Panther", "Static", "Spider-Man"],
+    answer: "Spider-Man"
   },
   {
-    title:"",
-    choices:["C1","C2","C3","C4"],
-    answer:"C3"
+    title: "",
+    choices: ["C1", "C2", "C3", "C4"],
+    answer: "C3"
   },
 ]
 var time = 60;
-var questionIndex= 0;//current question
+var currentQuestion = 0;//current question
 var timmerInterval;
-var highScores=[];
-var questionWindow= document.getElementById("container");
-var quizResults= document.getElementById("results")
-var Submitbutton=document.getElementById("Submit")
+var highScores = [];
+var questionWindow = document.getElementById("quiz");
+var quizResults = document.getElementById("results")
+var Submitbutton = document.getElementById("Submit")
 // event to capture the user start button to capture when the user clicks the start button that calls the start game function 
 //start game function 
 // 1. hide the start game start game screen and the show questions screen
 //2. it will start the timer
 //3. it will load the first question
 // 2/3 will be a new function 
-function startQuiz(){
+// quiz
+function startQuiz(questions, quizContainer, resulttsConatiner, sumbit) {
 
-  var output =[];
-  questions.forEach(currentQuestion,questionNumber){
-    var answers =[];
+  function showQuestions(questions, quizContainer) {
 
-    for(currentQuestion.answers){
-      answers.push(
-        <label>
-          <input type ="raido" name="question{questionNumber}" value={"letter"}>
-            {"letter"}:
-            {currentQuestion.answers[letter]}
-          </input>
-        </label>
-      )
+    var output = [];
+
+    var answers;
+
+    for(var i=0; i<questions.length; i++){
+
+      answers =[];
+
+      for(letter in questions[i].answers){
+        answers.push("<label>" + '<input type = "radio" name ="question" '+i+'"value="'+letter+'">' +letter + ';' + questions[i].answers[letter]+'</label>');
+      }
+      output.push(
+        '<div class="question">' + questions[i].question + '</div>'
+        + '<div class="answers">' + answers.join('')+ '</div>'
+      );
+    
+  };
+   quizContainer.innerHTML= output.join('');
+  
+
+  //will start when the user clicks submit
+  function showResults(questions, quizContainer, resulttsConatiner) {
+    var answerContainers =quizContainer.querySelectorAll('.answers');
+    var userAswer ='';
+    var numCorrect = 0;
+
+    for(var i=0; i<questions.length; i++){
+      userAswer =(answerContainers[i].querySelector('input[name=question'+i+']:checked'))|| {}.value;
+      if(userAswer === questions[i].correctAnswer){
+        numCorrect++;
+
+        answerContainers[i].style.color= "blue";
+      }
+      else{
+        answerContainers[i].style.color= "green";
+      }
     }
-    output.push(
-      <div class="question">{currentQuestion.questions}</div>
-      <div class="answer">{answers.join("")}</div>
+    resulttsConatiner.innerHTML = numCorrect + 'out of' + questions.length;
+  };
+  //show questions
+  showquestions(questions, quizContainer);
 
-    );
-  }
-}
-//will start when the user clicks submit
-function showResults(){
 
-}
+  submitButton.onClick = function () {
+    showResults(questions, quizContainer, resulttsConatiner)
 
-startQuiz();
-
-Submitbutton.addEventListener("click",showResults);
-
+  };
+};
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 //function to start the timer and to stop when time goes to 0
 
 
